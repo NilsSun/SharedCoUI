65b3b34314a81f6ee67a9a6bd715dca16c75ba9b
NilsSun <xs222ag@student.lnu.se>
2016-03-13 18:16:03 +0100

    milestone collectionList

--- js/app.js
+++ js/app.js
@@ -4,11 +4,21 @@ var App = angular.module('ScuiApp', ['ionic']);
 const server = 'https://localhost';
 
 App.run(function($rootScope, $http) {
-	//$http.get("http://localhost/archive/documents").success(
-    $http.get(`${server}/artifacts`).success(
+//    $http.get(`${server}/artifacts`).success(
+//			function(response) {
+//				$rootScope.metadataList = response;
+//			});
+
+    $http.get(`${server}/currentcollection`).success(
 			function(response) {
-				$rootScope.metadataList = response;
+                $rootScope.currentCollection = response;
 			});
+
+    $http.get(`${server}/collectionlist`).success(
+			function(response) {
+				$rootScope.metadataList = response.recordList;
+			});
+
 });
 
 App.directive('fileModel', [ '$parse', function($parse) {
@@ -38,36 +48,63 @@ App.controller('ListCtrl', [ '$scope', 'fileOperationServ', ListCtrlFunc]);
 
 function ArchiveListServiceFunc($http, $rootScope) {
 	this.search = function() {
-		$http.get(`${server}/artifacts`).success(
-            function(response) {
-			    $rootScope.metadataList = response;
-		    }).error(function() {
-		});
+//		$http.get(`${server}/artifacts`).success(
+//            function(response) {
+//			    $rootScope.metadataList = response;
+//		    }).error(function() {
+//		});
+
+    $http.get(`${server}/currentcollection`).success(
+			function(response) {
+                $rootScope.currentCollection = response;
+			});
+
+    $http.get(`${server}/collectionlist`).success(
+			function(response) {
+				$rootScope.metadataList = response.recordList;
+			});
+
 	}
 }
 
 function fileOperationServiceFunc($http, ArchiveListServ) {
-	this.uploadFileToUrl = function(uploadUrl, file, name, date) {
+	this.uploadFileToUrl = function(uploadUrl, file) {
 		var fd = new FormData();
 		fd.append('file', file);
-		fd.append('person', name);
-		fd.append('date', date);
 		$http.post(uploadUrl, fd, {
 			transformRequest : angular.identity,
 			headers : {
 				'Content-Type' : undefined
 			}
 		}).success(function() {
-			ArchiveListServ.search(null, null);
-		}).error(function() {
+			ArchiveListServ.search();
+		}).error(function(data, status, headers, config) {
+            alert(data.errorInfo);
 		});
-	}
+	};
+
+	this.updateFileToUrl = function(updateUrl, file) {
+		var fd = new FormData();
+		fd.append('file', file);
+		$http.post(updateUrl, fd, {
+			transformRequest : angular.identity,
+			headers : {
+				'Content-Type' : undefined
+
+			}
+		}).success(function() {
+			ArchiveListServ.search();
+		}).error(function(data, status, headers, config) {
+            //alert(data.errorInfo);
+		});
+	};
 
     this.deleteFileByName = function(name) {
         $http.get(`${server}/artifactD/${name}`).success(function() {
 			ArchiveListServ.search();
 		}).error(function() {
 		});
+
 //        $http({
 //              method: "DELETE",
 //              url: "https://localhost/artifact/" + name,
@@ -80,22 +117,49 @@ function fileOperationServiceFunc($http, ArchiveListServ) {
 //		}
 //        );
 
-    }
+    };
+
+    this.goToCollectionToUrl = function(goToUrl,name,key) {
+		var fd = new FormData();
+        fd.append('Key', key);
+        fd.append('Name', name);
+		$http.post(goToUrl, fd, {
+			transformRequest : angular.identity,
+			headers : {
+				'Content-Type' : undefined
+			}
+		}).success(function() {
+			ArchiveListServ.search();
+		}).error(function(data, status, headers, config) {
+            alert(data.errorInfo);
+		});
+    };
 
 }
 
 function ListCtrlFunc($scope, fileOperationServ) {
     $scope.uploadFile = function() {
         var file = $scope.myFile;
-        var name = "name";
-        var date = "2016-03-04";
         console.log('file is ' + JSON.stringify(file));
-        var uploadUrl = `${server}/archive/upload`;
-        fileOperationServ.uploadFileToUrl(uploadUrl, file, name, date);
+        var uploadUrl = `${server}/artifact`;
+        fileOperationServ.uploadFileToUrl(uploadUrl, file);
+    };
+
+    $scope.updateFile = function(index) {
+        var file = $scope.myFile;
+        console.log('file is ' + JSON.stringify(file));
+        var name = $scope.metadataList[index].name;
+        var updateUrl = `${server}/artifact/${name}`;
+        fileOperationServ.updateFileToUrl(updateUrl, file);
     };
 
     $scope.deleteFile = function(index) {
         fileOperationServ.deleteFileByName($scope.metadataList[index].name);
+    };
+
+    $scope.goToCollection = function(index) {
+        var goToUrl = `${server}/setcurrentcollection`;
+        fileOperationServ.goToCollectionToUrl(goToUrl, $scope.metadataList[index].name, $scope.metadataList[index].key);
     };
 
 //    $scope.moredata = false;
